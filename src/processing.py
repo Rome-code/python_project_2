3948852df845077fcae23c602e226ceefcc4fcf4
Rim Faritov <faritov.rome@gmail.com>
2024-09-12 18:19:25 +0500

    add processing.py with two functions

---
+++ src/processing.py
@@ -0,0 +1,6 @@
+def filter_by_state() -> list:
+    pass
+
+
+def sort_by_date() -> list:
+    pass
--- src/widget.py
+++ src/widget.py
@@ -1,20 +1,32 @@
 from masks import get_mask_account, get_mask_card_number
 
+"""Ввод номера карты или номера счета"""
 account_type_card_num = input()
 
 
 def mask_account_card(account_type_card_num: str) -> str:
+    """Функция, для выводв номера карты или счета с текстом"""
     account_type_card_num_list = account_type_card_num.split(" ")
     if len(account_type_card_num_list[-1]) < 20:
         mask_card_number = get_mask_card_number(account_type_card_num_list[-1])
         account_type_card_num_list[-1] = mask_card_number
-
-        return " ".join(account_type_card_num_list)
     else:
         mask_account = get_mask_account(account_type_card_num_list[-1])
         account_type_card_num_list[-1] = mask_account
-        return " ".join(account_type_card_num_list)
+    return " ".join(account_type_card_num_list)
+
+
+"""Ввод строки с датой и временем"""
+date_and_time = input()
+
 
 def get_date(date_and_time: str) -> str:
-    pass
+    """Функция, реобразующая date_and_time в ДД.ММ.ГГГГ"""
+    if len(date_and_time) == 26 and date_and_time[4] == "-" and date_and_time[7] == "-" and date_and_time[-7] == ".":
+        nec_form_date = f"{date_and_time[8:10]}.{date_and_time[5:7]}.{date_and_time[0:4]}"
+    return nec_form_date
+
+
+"""Вызов и возвращение результатов функций"""
 print(mask_account_card(account_type_card_num))
+print(get_date(date_and_time))
