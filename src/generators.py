0e1aec3ba175013f36f846427aa49e698671d8b1
Rim Faritov <faritov.rome@gmail.com>
2024-09-29 16:57:58 +0800

    add two generators in generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,8 +1,32 @@
-def filter_by_currency():
+from tests.conftest import transactions
+
+
+def filter_by_currency(transaction_list, currency):
+    for transaction in transaction_list:
+        if transaction.get("operationAmount", [{}]).get("currency", [{}]).get("code", [{}]) == currency:
+            yield transaction
+
+
+usd_transactions = filter_by_currency(transactions, "USD")
+try:
+    for i in range(len(transactions)):
+        print(next(usd_transactions))
+
+except StopIteration:
     pass
 
-def transaction_descriptions():
+
+def transaction_descriptions(transaction_list):
+    for transaction in transaction_list:
+        yield transaction.get("description")
+
+
+descriptions = transaction_descriptions(transactions)
+for _ in range(len(transactions)):
+    print(next(descriptions))
+
+
+def card_number_generator(start, stop):
     pass
 
-def card_number_generator():
-    pass
+
